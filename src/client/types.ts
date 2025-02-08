2eb39bcefc4d8c948625811bb01306e6990d35d6
Roy OSSAI <roy.ossai@bpifrance.fr>
2025-02-08 23:20:54 +0100

    reinforced the client schema

--- src/client/types.ts
+++ src/client/types.ts
@@ -1,5 +1,19 @@
 export interface StreamsProperties {
-    [key: string]: string;
+    'ksql.streams.auto.offset.reset'?: 'earliest' | 'latest';
+    'ksql.query.pull.table.scan.enabled'?: boolean;
+    'ksql.query.pull.max.allowed.offset.lag'?: number;
+    'processing.guarantee'?: 'at_least_once' | 'exactly_once';
+    'cache.max.bytes.buffering'?: number;
+    'ksql.streams.cache.max.bytes.buffering'?: number;
+    'timestamp.format'?: string;
+    'timestamp.extractors'?: string;
+    'ksql.streams.state.dir'?: string;
+    'ksql.persistent.prefix'?: string;
+    'num.stream.threads'?: number;
+    'commit.interval.ms'?: number;
+    'ksql.streams.error.max.interval.ms'?: number;
+    'ksql.streams.retry.backoff.ms'?: number;
+    [key: string]: string | number | boolean | undefined;
 }
 
 export interface KsqlDBRequestConfig {
